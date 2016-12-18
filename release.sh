e157dc1abe1a8df11f03c3eb4d9574bbe84ab965
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-18 17:32:53 +0530

    Initial commit
    release.sh, to --/release/-- the builds after successful completion of "Grunt-Task(s):build", for "SCGDB®".
    Usage: $ sh release.sh

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# ==========================++== SCGDB® ==++==========================
+#           A manually curated dedicated schizosaccharomyces
+#                     comparative genome database.
+#
+#                                 CCPE
+#             Centre for Computational Protein Engineering
+#
+#                            Apache License
+#                    http://www.apache.org/licenses/
+#
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+# ====================================================================
