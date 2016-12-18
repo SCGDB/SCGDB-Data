6f9d2aafbc619609aba6cb4cbdb9d17b48b58efb
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-18 21:11:52 +0530

    Initial commit
    Reading {./citation.json} from the —//root//— to get the citation of — SCGDB®.
    Note: Removing byte order marker.
    SEED™: Grunt based build system. | See: http://seed.sequomics.com/.

--- index.js
+++ index.js
@@ -80,3 +80,10 @@ function stripBOM(content) {
   }
   return content;
 }
+
+///--------------------
+// A citation of SCGDB®
+///--------------------
+var cite = JSON.parse(stripBOM(fs.readFileSync('./citation.json', {
+  encoding: "utf8"
+})));
