4a8ff5c82d373c42c38c2c45e35f7dbe3827927b
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-18 18:18:01 +0530

    Initial commit
    grunt-contrib-clean — Clear files and directories.
    Usage: $ npm install grunt-contrib-clean --save-dev | Help: https://github.com/gruntjs/grunt-contrib-clean

---
+++ tasks/options/clean.js
@@ -0,0 +1,30 @@
+/*!
+ * -----------
+ * SCGDB®-Data
+ * -----------
+ * Data of SCGDB — A manually curated dedicated schizosaccharomyces comparative genome database.
+ * ______________________________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ______________________________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ______________________________________________________________________________________________
+ *
+ * @date      : 18-Dec-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-clean
+ * @build     : SEED™ — Skövde
+ *              └────── A Sequømics Product — http://seed.sequomics.com/.
+ * ______________________________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "SCGDB®-Data".
+ * ______________________________________________________________________________________________
+ */
